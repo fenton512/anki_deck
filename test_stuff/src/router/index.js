7e8aeaf1c2c1e14df1801f7a3d4f0a8ba5a17509
fenton512 <viktorken8264@gmail.com>
2025-06-15 14:34:45 +0300

    It is deployed version without styles
    rewrited package.json, index.js, and vite.config.js to deployed site on github pages

--- test_stuff/src/router/index.js
+++ test_stuff/src/router/index.js
@@ -1,15 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import WelcomPage from '@/pages/welcomPage.vue'
-import GenerSettings from '@/pages/generSetPage.vue'
+import { createRouter, createWebHashHistory } from 'vue-router';
+import WelcomPage from '@/pages/welcomPage.vue';
+import GenerSettings from '@/pages/generSetPage.vue';
 import PasteTextPage from '@/pages/pasteTextPage.vue';
+
 const routes = [
   { path: '/', name: 'Welcom', component: WelcomPage },
-  { path: '/generSettings', name: 'GenerSettings', component:GenerSettings},
-  {path: '/pasteText', name:'PasteText', component: PasteTextPage},
+  { path: '/generSettings', name: 'GenerSettings', component: GenerSettings },
+  { path: '/pasteText', name: 'PasteText', component: PasteTextPage },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory('/anki_deck/'),
   routes
 });
 
