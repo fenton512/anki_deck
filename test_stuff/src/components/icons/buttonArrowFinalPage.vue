8c27358b61da4578bda44de02b69312560f55c12
fenton512 <viktorken8264@gmail.com>
2025-06-19 11:59:46 +0300

    Write structure for finalPage with visualization
    It is bad scaled, but desctop version looks fine

---
+++ test_stuff/src/components/icons/buttonArrowFinalPage.vue
@@ -0,0 +1,5 @@
+<template>
+    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M16 2V30M16 30L30 16M16 30L2 16" stroke="white" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+</template>
