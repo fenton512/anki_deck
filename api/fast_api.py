41e21a23c84ce6af274851d2e80674ac5f19aae9
fishkadealer229 <midav.06@mail.ru>
2025-06-19 04:00:20 +0300

    API and Database first commit

---
+++ api/fast_api.py
@@ -0,0 +1,112 @@
+from fastapi import FastAPI, Query
+from sqlite3 import connect
+import os
+from decryptors import *
+
+app = FastAPI()
+basedir = os.path.abspath(os.path.dirname(__file__))
+data_file = os.path.join(basedir, 'api/anki_deck.db')
+con = connect(data_file)
+cur = con.cursor()
+
+@app.get("/user/get")
+async def get_user(login: str):
+    ans = {
+    'id': cur.execute("SELECT user_id FROM users WHERE login = ?", (login,)).fetchone()[0],
+    'login': login,
+    'encrypted_password': cur.execute("SELECT encrypted_password FROM users WHERE login = ?",
+                                      (login,)).fetchone()[0]
+    }
+    return ans
+
+
+@app.post("/user/post")
+async def post_user(login: str, encrypted_password: str):
+    cur.execute("INSERT INTO users (login, encrypted_password) VALUES (?, ?)", (login, encrypted_password))
+    con.commit()
+    return {"status": 'ok'}
+
+
+@app.get("/deck/get")
+async def get_deck(deck_id: int):
+    ans = {
+        'deck_id': deck_id,
+        'cards': decode_blob(cur.execute("SELECT cards FROM decks WHERE deck_id = ?",
+                                         (deck_id,)).fetchall()[0]),
+        'user_id': cur.execute("SELECT user_id FROM decks WHERE deck_id = ?", (deck_id,)).fetchone()[0]
+    }
+    return ans
+
+
+@app.post("/deck/post")
+async def post_deck(user_id: int, cards: list[int] = Query()):
+    cur.execute("INSERT INTO decks (user_id, cards) VALUES (?, ?)", (user_id, array_to_blob(cards)))
+    con.commit()
+    return {"status": 'ok'}
+
+
+@app.get("/card/get")
+async def get_card(card_id: int):
+    ans = {
+        'card_id': card_id,
+        'word_id': cur.execute("SELECT word_id FROM cards WHERE card_id = ?", (card_id,)).fetchone()[0],
+        'sentences': decode_blob(cur.execute("SELECT sentences FROM cards WHERE card_id = ?",
+                                         (card_id,)).fetchall()[0])
+    }
+    return ans
+
+
+@app.post("/card/post")
+async def post_card(word_id: int,  sentences: list[str] = Query()):
+    cur.execute("INSERT INTO cards (word_id, sentences) VALUES (?, ?)", (word_id, array_to_blob(sentences)))
+    con.commit()
+    return {"status": 'ok'}
+
+
+@app.get("/word/get_by_word")
+async def get_by_word(word: str):
+    ans = {
+        'word_id': cur.execute("SELECT word_id FROM words WHERE word = ?", (word,)).fetchone()[0],
+        'word': word,
+        'translations': cur.execute("SELECT translations FROM words WHERE word = ?",
+                                    (word,)).fetchone()[0],
+        'context_sentence': cur.execute("SELECT context_sentence FROM words WHERE word = ?",
+                                        (word,)).fetchone()[0],
+        'is_important': bool(cur.execute("SELECT is_important FROM words WHERE word = ?",
+                                    (word,)).fetchone()[0]),
+        'user_id': cur.execute("SELECT user_id FROM words WHERE word = ?",
+                               (word,)).fetchone()[0]
+    }
+    return ans
+
+
+@app.get("/word/get_by_id")
+async def get_by_id(word_id: int):
+    ans = {
+        'word_id': word_id,
+        'word': cur.execute("SELECT word FROM words WHERE word_id = ?", (word_id,)).fetchone()[0],
+        'translations': cur.execute("SELECT translations FROM words WHERE word_id = ?",
+                                    (word_id,)).fetchone()[0],
+        'context_sentence': cur.execute("SELECT context_sentence FROM words WHERE word_id = ?",
+                                        (word_id,)).fetchone()[0],
+        'is_important': bool(cur.execute("SELECT is_important FROM words WHERE word_id = ?",
+                                    (word_id,)).fetchone()[0]),
+        'user_id': cur.execute("SELECT user_id FROM words WHERE word_id = ?",
+                               (word_id,)).fetchone()[0]
+    }
+    return ans
+
+
+@app.post("/word/post")
+async def post_word(word: str, translations: str, context_sentence: str, is_important: bool, user_id: int, mode: str):
+    cur.execute("INSERT INTO words (word, translations, context_sentence, is_important, user_id)"
+                " VALUES (?, ?, ?, ?, ?)", (word, translations, context_sentence, int(is_important), user_id))
+    word_id = cur.execute("SELECT word_id FROM words").fetchall()[-1][0]
+    if mode == "known":
+        cur.execute("INSERT INTO known_words (word_id) VALUES (?)", (word_id,))
+    elif mode == "unknown":
+        cur.execute("INSERT INTO unknown_words (word_id) VALUES (?)", (word_id,))
+    else:
+        cur.execute("INSERT INTO unwanted_words (word_id) VALUES (?)", (word_id,))
+    con.commit()
+    return {"status": 'ok'}
