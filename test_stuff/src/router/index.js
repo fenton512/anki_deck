a581ef2176dd6e1cac385b0c0eb88a759c46be36
fenton512 <viktorken8264@gmail.com>
2025-06-19 10:29:46 +0300

    Create finalPage and update ./router/index.js

--- test_stuff/src/router/index.js
+++ test_stuff/src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 import WelcomPage from '@/pages/welcomPage.vue';
 import GenerSettings from '@/pages/generSetPage.vue';
 import PasteTextPage from '@/pages/pasteTextPage.vue';
+import FinalPage from '@/pages/finalPage.vue';
 
 const routes = [
   { path: '/', name: 'Welcom', component: WelcomPage },
   { path: '/generSettings', name: 'GenerSettings', component: GenerSettings },
   { path: '/pasteText', name: 'PasteText', component: PasteTextPage },
+  { path: '/result', name: 'FinalPage', component: FinalPage}
 ];
 
 const router = createRouter({
