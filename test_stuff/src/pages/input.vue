48de3ddb7c70e7db37bbee825f0c208fb283abea
vanyaspapayas <jsugarpork@gmail.com>
2025-06-20 19:19:34 +0300

    Added input.vue

---
+++ test_stuff/src/pages/input.vue
@@ -0,0 +1,87 @@
+
+<script> 
+import BaseButton from '@/components/Basebutton.vue';
+import router from '@/router';
+export default {
+    mounted() { 
+    },
+    components: {
+        BaseButton
+    },
+    methods: {
+    }
+}
+</script>
+
+<template>
+   <main>
+        <div class = "header">Загрузите файл в формате .PDF</div>
+        <div class = "form-container">
+            <form>
+                <textarea id = "text" placeholder="Например: Deftones - Sextape"></textarea>
+            </form>
+        </div>
+        <BaseButton class = "submit">Сгенерировать деку</BaseButton>
+   </main>
+</template>
+
+<style scoped> 
+    main {
+        margin: 0 px;
+        font-family: "Inter", sans-serif;
+        font-optical-sizing: auto;
+        font-weight: 300;
+        font-style: normal;
+        color: white;
+        font-size: 17px;
+    }
+    .header {
+        font-family: "Roboto", sans-serif;
+        font-optical-sizing: auto;
+        font-weight: 400;
+        font-style: normal;
+        font-variation-settings:      
+        "wdth" 100;
+        text-align: center;
+        font-size: 32px;
+    }
+    .form-container {
+        align-self: center;
+        width: 600px;
+        margin: 0 auto;
+        margin-top:20px;
+    }
+    #text {
+        background-color: transparent;
+        border-radius: 10px;
+        padding-top:0.5em;
+        height: 150px;
+        border: 1px solid whitesmoke;
+        width: 100%;
+        color: white;
+        box-sizing: border-box;
+        padding-left: 0.5em;
+        font-size: 14px;
+        resize: vertical;
+    }
+    #text::placeholder {
+        text-align: left;
+        color: white;
+        opacity: 0.8;
+    }
+    #text:focus {
+        outline: none;
+        box-shadow: none;
+    }
+    .submit {
+        align-self: center;
+        width: 255px;
+        height: 60px;
+        border-radius: 0;
+        border: 1px solid whitesmoke;
+        margin: 0 auto; 
+        margin-top: 300px;
+        
+    }
+    
+</style>
