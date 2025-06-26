b4d593c2a10fe8cce7e9c1a23d6a18270a1bbeb2
fenton512 <viktorken8264@gmail.com>
2025-06-26 19:50:04 +0300

    [abd] Wrote basic structure for filtering page
    -text does not save string transfer

--- test_stuff/src/pages/filterFromText.vue
+++ test_stuff/src/pages/filterFromText.vue
@@ -1,5 +1,6 @@
 <script>
 import { useUserTextStore } from '@/stores/userText';
+import Basebutton from '@/components/Basebutton.vue';
 export default {
     data() {
         return {
@@ -15,6 +16,9 @@ export default {
         this.text = this.textStore.text;
         this.textArea = document.getElementsByClassName("textarea")[0];
         this.validateWords();
+    },
+    components: {
+        Basebutton
     },
     methods: {
         validateWords() {
@@ -51,17 +55,23 @@ export default {
 </script>
 
 <template>
-    <div class="textarea" >
-        <span v-for="(word, index) in words">
-        <span>{{word.extrChars[0]}}</span>
-        <span
-        :key="index"
-        :class="word.class"
-        @click="this.changeClass(index)">
-            {{word.word}}
-        </span>
-        <span>{{word.extrChars[1]}}</span>
-        </span>
+    <div class="main-container">
+        <h1>Выделение слов</h1>
+        <div class="text-area" >
+            <span v-for="(word, index) in words">
+            <span>{{word.extrChars[0]}}</span>
+            <span
+            :key="index"
+            :class="word.class"
+            @click="this.changeClass(index)">
+                {{word.word}}
+            </span>
+            <span>{{word.extrChars[1]}}</span>
+            </span>
+        </div>
+        <div class="button-container">
+            <Basebutton class="start-generation">Начать генерацию</Basebutton>
+        </div>
     </div>
 </template>
 
@@ -74,5 +84,24 @@ export default {
     }
     .neverLearn{
         background-color: #B74747;
+    }
+    .main-container{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        height: 100%;
+    }
+    .text-area{
+        flex-grow: 8;
+    }
+    .h1{
+        flex-grow: 1;
+    }
+    .button-container{
+        flex-grow: 1;
+        /* height: 50%; */
+    }
+    .start-generation{
+        height: 70%;
     }
 </style>
