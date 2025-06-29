e8f33c284c7532f794ae6045454b4c92e44191dd
fenton512 <viktorken8264@gmail.com>
2025-06-29 18:12:52 +0300

    [fix] rewrite structure for state in API store

--- test_stuff/src/stores/API.js
+++ test_stuff/src/stores/API.js
@@ -2,15 +2,15 @@ import { defineStore } from "pinia";
 
 export const useAPIStore = defineStore("API", {
     state: () => ({
-        wantLearn: [],
-        dontWantLearn: [],
-        count: 0
+        data: {
+            wantLearn: [],
+            dontWantLearn: [],
+            count: 0
+        }
     }),
     actions: {
         setState(object) {
-            this.wantLearn = object.wantLearn;
-            this.dontWantLearn = object.dontWantLearn;
-            this.count = object.count;
+            this.data = object
         }
     },
     persist: {
@@ -19,7 +19,7 @@ export const useAPIStore = defineStore("API", {
             {
                 key: "API",
                 strorage: sessionStorage,
-                paths: ['wantLearn', 'dontWantLearn', 'count']
+                paths: ['data']
             }
         ]
     }
