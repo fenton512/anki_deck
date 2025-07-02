f0250cbdf98bce8c96f6d59ec4e4b2fe3e0af6ff
fenton512 <viktorken8264@gmail.com>
2025-07-02 21:28:26 +0300

    Add new page file and update router

---
+++ test_stuff/src/pages/listFilter.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>This is List page</h1>
+</template>
--- test_stuff/src/router/index.js
+++ test_stuff/src/router/index.js
@@ -1,21 +1,21 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import WelcomPage from '@/pages/welcomPage.vue';
-import GenerSettings from '@/pages/generSetPage.vue';
 import Filter from '@/pages/filter.vue';
 import FinalPage from '@/pages/finalPage.vue';
 import Input from '@/pages/input.vue';
 import Tinder from '@/pages/tinder.vue';
-import FilterFromText from '@/pages/filterFromText.vue'
+import FilterFromText from '@/pages/filterFromText.vue';
+import FilterList from '@/pages/listFilter.vue';
 
 
 const routes = [
   { path: '/', name: 'Welcom', component: WelcomPage },
-  { path: '/generSettings', name: 'GenerSettings', component: GenerSettings },
   { path: '/filter', name: 'Filter', component: Filter },
   { path: '/result', name: 'FinalPage', component: FinalPage},
   { path: '/input', name: 'Input', component: Input},
-  { path: '/tinder', name: 'Tinder', component: Tinder},,
-  { path: '/filterText', name: 'FilterFromText', component: FilterFromText}
+  { path: '/tinder', name: 'Tinder', component: Tinder},
+  { path: '/filterText', name: 'FilterFromText', component: FilterFromText},
+  { path: '/filterList', name: 'List', component: FilterList}
 ];
 
 const router = createRouter({
