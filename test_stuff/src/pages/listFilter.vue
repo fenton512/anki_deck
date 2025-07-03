b139d3ab39a092d13edf0b1357b0b7bacbe5eb35
fenton512 <viktorken8264@gmail.com>
2025-07-03 21:52:09 +0300

    [feat] Transform translation for cards works without animation and good resting

--- test_stuff/src/pages/listFilter.vue
+++ test_stuff/src/pages/listFilter.vue
@@ -1,3 +1,170 @@
+<script>
+import Basebutton from '@/components/Basebutton.vue';
+import { useAPIStore } from '@/stores/API';
+
+export default {
+    data() {
+        return {
+            isVisible: false,
+            cardNumStart: 1,
+            cardNumTotal: 0,
+            counter: 0,
+            wordList: ["word1", "word2", "word3", "word4", "word5", "word6", "word7", "word8"],
+            scrollerWidth: 0,
+            cardWidth: 0,
+            gap: 0,
+            currentPage: 1,
+            maxPage: 0,
+            direction: null, // 0 - next page for translation, 1 - previous page for translation
+        }
+    },
+    components: {
+        Basebutton
+    },
+    methods: {
+        translate() {
+            let distance = (this.cardWidth+this.gap)*4;
+            switch (this.direction) {
+                case 0:
+                    this.$refs.scroller.style.transform = `translateX(-${(this.currentPage-1)*distance}px)`;
+                    break;
+                case 1:
+                    this.$refs.scroller.style.transform = `translateX(+${(this.currentPage-1)*distance}px)`;
+                    break;
+                
+            }
+        },
+        moveForward(){
+            this.direction = 0; 
+            if (this.currentPage < this.maxPage) 
+                this.currentPage++
+        },
+        moveBack(){
+            this.direction = 1;
+            if (this.currentPage > 1)
+                this.currentPage--
+        },
+        updateSizes(){
+            if (!this.$refs.card[0] || !this.$refs.scroller || this.$refs.card[0].length === 0)
+                return;
+            this.gap = parseFloat(window.getComputedStyle(this.$refs.scroller).gap);
+            this.cardWidth = this.$refs.card[0].offsetWidth;
+            this.scrollerWidth = this.$refs.scroller.offsetWidth;
+            let cardNum = this.wordList.length;
+            // this.maxPage = Math.floor((this.cardWidth*cardNum+(cardNum-1)*this.gap) / ((this.cardWidth+this.gap)*4))
+            this.maxPage = Math.ceil(this.wordList.length/4)
+            
+        }
+    },
+    mounted() {
+        this.updateSizes();
+        window.addEventListener("resize", this.updateSizes);
+    },
+    unmounted(){
+        window.removeEventListener('resize', this.updateSizes);
+    },
+    watch: {
+        currentPage(){
+            this.translate()
+        }
+    },
+}
+</script>
+
 <template>
-    <h1>This is List page</h1>
+    <div class="main-container">
+        <h1>Выбор слов из текста
+            <span class="question-mark" @mouseover="isVisible=true" @mouseout="isVisible=false">?
+                <span v-if="isVisible" class="user-hint">Кликни по карточке со словом, чтобы задать ему статус:<br><span style="background-color: #71c686;">Зеленые карточки:</span>
+                неизвестные слова, которые ты хочешь учить<br>
+                <span style="background-color: #B74747;">Красные карточки:</span> слова, которые ты не хочешь учить</span>
+            </span>
+        </h1>
+        <div class="nested-container">
+            <span class="card-number">Слова {{cardNumStart}}-{{cardNumStart+4}} из {{cardNumTotal}}</span>
+            <div class="card-container">
+                <div class="scroller" ref="scroller">
+                    <div ref="card" class="card" v-for="word, index in wordList" :key="index">{{word}}</div>
+                </div>
+            </div>
+            <span class="card-picked">Выбрано слов {{counter}} из {{cardNumTotal}}</span>
+            <Basebutton @click="moveForward">Прокрутка вперед</Basebutton>
+            <Basebutton @click="moveBack"> Прокрутка назад</Basebutton>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+    .main-container{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        height: 100%;
+    }
+    h1{
+        flex: 0 0 auto;
+        font-size: 30px;
+        position: relative;
+        font-weight: 400;
+        font-size: 40px;
+    }
+    .question-mark{
+        border: #888 solid;
+        border-radius: 24px;
+        display: inline-block;
+        width: 28px;
+        height: 28px;
+        text-align: center;
+        position: absolute;
+        top: 5px;
+        right: -42px;
+        font-size: 22px;
+    }
+    .user-hint{
+        content: attr(data-text);
+        min-width: 420px;
+        z-index: 1;
+        background-color: var(--color-background);
+        position: absolute;
+        border-radius: 10px;
+        border: white solid;
+        border-width: 1px;
+        top: 35px;
+        font-size: 18px;
+        font-weight: 400;
+        left: 30px;
+    }
+    .nested-container{
+        display: flex;
+        align-content: start;
+        flex-direction: column;
+        width: 932px;
+    }
+    .card-container{
+        display: flex;
+        width:932px;
+        flex: 0 0 auto;
+        overflow: hidden;
+        /* transition: ; */
+
+    }
+    .card-number{
+        font-size: 20px;
+    }
+    .card{
+        height: 260px;
+        width: 214px;
+        box-sizing: border-box;
+        border-radius: 28px;
+        border: 2px solid #fff;
+        font-size: 40px;
+        flex-shrink: 0;
+    }
+    .scroller{
+        display: flex;
+        gap: 25px;
+        flex-wrap:nowrap;
+        width: 100%;
+
+    }
+</style>
