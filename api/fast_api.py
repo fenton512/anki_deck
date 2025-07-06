c81b28eaa97b330c2567cdd8a9439ead0a24d6c9
fenton512 <viktorken8264@gmail.com>
2025-07-06 21:54:48 +0300

    verson for docker

--- api/fast_api.py
+++ api/fast_api.py
@@ -18,7 +18,7 @@ cur = con.cursor()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "http://localhost:8000/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
